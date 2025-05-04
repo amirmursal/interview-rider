d76340f79d94988f51ff6712e7f7a68ba527cc90
Amir Mursal <amirmursal@microsoft.com>
2025-05-04 23:26:00 +0530

    add polyfill for bind method

--- JS_Concepts/polyfill_apply_method.js
+++ JS_Concepts/polyfill_apply_method.js
@@ -3,15 +3,6 @@
  * The apply() method calls a function with a given this value and arguments provided as an array (or an array-like object).
  */
 
-/*function testApply(mutlipler) {
-  const age = this.age;
-  return age * mutlipler;
-}
-
-const user = { age: 25 };
-
-console.log(testApply.apply(user, [78]));*/
-
 function displayData(age) {
   console.log(`This is his name ${this.data} with age ${age}`);
 }
--- JS_Concepts/polyfill_bind_method.js
+++ JS_Concepts/polyfill_bind_method.js
@@ -3,36 +3,25 @@
  * The bind() method creates a new function that, when called, has its this keyword set to the provided value.
  * It allows you to set the value of this for a function, regardless of how that function is called.
  * It is a built-in method of the Function prototype in JavaScript.
- *
- * Implement a polyfill for the Function.prototype.bind method.
- * The bind method creates a new function that, when called, has its this keyword set to the provided value.
- * example:
- * function myFunction() {
- *  console.log(this.name);
- * }
- * const obj = { name: 'John' };
- * const boundFunction = myFunction.bind(obj);  // Output: John
- * boundFunction(); // Output: John
  */
 
-function calculateAge(multiple) {
-  const age = this.age * multiple;
-  console.log(age);
+function displayData(age) {
+  console.log(`This is his name ${this.data} and age ${age}`);
 }
 
-const obj = { age: 23 };
+const userData = {
+  data: "Amir",
+};
 
-//const test = calculateAge.bind(obj, 34);
-//test(); // you have to call explicitly call bind method
+//const test = displayData.bind(userData, 25);
+//test();
 
-Function.prototype.customBind = function (thisArg, ...argArray) {
-  // how bind works it will return a method
-  const originalMethod = this;
+Function.prototype.customBind = function (thisArg, ...funargs) {
+  const func = this;
   return function (...args) {
-    //return originalMethod.apply(thisArg, [...argArray, ...args]); // using apply method
-    return originalMethod.call(thisArg, ...argArray, ...args); // using call method
+    return func.call(thisArg, ...funargs, ...args);
   };
 };
 
-const test = calculateAge.customBind(obj, 34);
+const test = displayData.customBind(userData, 25);
 test();
