912748ffabeade5bfd7fc350d78bc8a2a7249a88
Amir Mursal <amirmursal@microsoft.com>
2025-05-04 18:03:22 +0530

    add hoisting explaination

--- JS_Concepts/hoisting.js
+++ JS_Concepts/hoisting.js
@@ -1,7 +1,11 @@
-getName();
-console.log(x);
-const x = 7;
+// whats is Hoisting in javascript?
+// Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of
+// their containing scope during the compile phase. This means that you can use variables
+// and functions before they are declared in the code.
 
-function getName() {
-  console.log("Inside Get Name");
-}
+//console.log(a);
+var a; // var is hoisted with undefined value
+//console.log(b);
+let b; // in case of let it is hoisted but not initilized
+console.log(c);
+const c = 1; // in case of const it is hoisted but not initilized
