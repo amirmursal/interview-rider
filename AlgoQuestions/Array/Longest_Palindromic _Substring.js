d3eefcd5ad6b0620efdcbf4faca9e385f990eaa1
Amir Mursal <amirmursal@microsoft.com>
2025-05-04 17:02:40 +0530

    add optimise panlindorm question

--- AlgoQuestions/Array/Longest_Palindromic _Substring.js
+++ AlgoQuestions/Array/Longest_Palindromic _Substring.js
@@ -2,34 +2,34 @@
  * Given a string str, return the longest contiguous substring that is a palindrome.
  * If there are multiple such substrings, return any one of them.
  * A palindrome is a sequence of characters that reads the same forward and backward (e.g., racecar).
- * A substring is any contiguous sequence of characters within a string. For example, the substrings of string abc are a, b, c, ab, bc, and abc. 
+ * A substring is any contiguous sequence of characters within a string. For example, the substrings of string abc are a, b, c, ab, bc, and abc.
  * A substring is formed by selecting a starting and ending point without skipping characters in between.
  * example:
  * Input: str = "cauumzssz"
-Output: "zssz"
-Explanation: 'zssz' is the longest palindromic substring within 'cauumzssz'.
-Input: str = "racecar"
-Output: "racecar"
-Explanation: The entire string 'racecar' is a palindrome and is the longest.
-Input: str = "cacae"
-Output: "cac"
-Explanation: Both 'cac' and 'aca' are valid palindromic substrings of the same length, and either can be returned.
+ * Output: "zssz"
+ * Explanation: 'zssz' is the longest palindromic substring within 'cauumzssz'.
  */
 
 const getLongestPalindromicString = (str) => {
-  let maxLength = 0;
   let result = "";
 
   for (let i = 0; i < str.length; i++) {
-    for (let j = i + 1; j <= str.length; j++) {
-      let sub = str.slice(i, j);
-      if (sub === sub.split("").reverse().join("") && sub.length > maxLength) {
-        result = sub;
-        maxLength = sub.length;
-      }
-    }
+    expand(i, i);
+    expand(i, i + 1);
   }
 
+  function expand(left, right) {
+    //We are still inside the string on both sides (left ≥ 0, right < length)
+    // The characters at both ends match (str[left] === str[right])
+    //If both conditions are true, it means the substring from left to right is still a palindrome — so we can try to expand it further.
+    while (left >= 0 && right < str.length && str[left] === str[right]) {
+      if (right - left + 1 > result.length) {
+        result = str.slice(left, right + 1);
+      }
+      left--;
+      right++;
+    }
+  }
   return result;
 };
 
