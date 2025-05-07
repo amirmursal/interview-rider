07bdae70f9f0bc13bc9ae3547dc33df23bf99c37
Amir Mursal <amirmursal@microsoft.com>
2025-05-07 21:16:09 +0530

    removed some files

---
+++ Questions/TypeScript_Questions.md
@@ -0,0 +1,1 @@
+1. Difference between type and interface in TypeScript
--- Questions/usageOfContext.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-
-const Button = () => {
-  // Theme conetxt needs to imported from other file
-
-  const theme = useContext(ThemeContext);
-
-  return <button>Click Me</button>;
-};
